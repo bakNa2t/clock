efa8463d4196d3ff10e2fcb29b00189700f30244
bakNa2t <ok2.lock@gmail.com>
2023-12-25 10:45:34 +0300

    Update time style

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 setInterval(() => {
-    let hours = document.getElementById('hours');
-    let minutes = document.getElementById('minutes');
-    let seconds = document.getElementById('seconds');
-    
-    let ampm = document.getElementById('ampm');
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
 
-    let hh = document.getElementById('hh');
-    let mm = document.getElementById('mm');
-    let ss = document.getElementById('ss');
+  let ampm = document.getElementById("ampm");
 
-    let hr_dot = document.querySelector('.hr_dot');
-    let min_dot = document.querySelector('.min_dot');
-    let sec_dot = document.querySelector('.sec_dot');
+  let hh = document.getElementById("hh");
+  let mm = document.getElementById("mm");
+  let ss = document.getElementById("ss");
 
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
+  let hr_dot = document.querySelector(".hr_dot");
+  let min_dot = document.querySelector(".min_dot");
+  let sec_dot = document.querySelector(".sec_dot");
 
-    let am = h >= 12 ? "PM" : "AM";
-    // shift 24 hr to 12 hr cklock
-    if (h > 12) {
-        h = h -12
-    };
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
 
-    // add zero before single digit number
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+  let am = h >= 12 ? "PM" : "AM";
+  // shift 24 hr to 12 hr cklock
+  if (h > 12) {
+    h = h - 12;
+  }
 
-    hours.innerHTML = h + "<br/><span>Hours</span>";
-    minutes.innerHTML = m + "<br/><span>Minutes</span>";
-    seconds.innerHTML = s + "<br/><span>Seconds</span>";
+  // add zero before single digit number
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
-    ampm.innerHTML = am;
+  //set current time and description
+  hours.innerHTML = h + "<br/><span>Hours</span>";
+  minutes.innerHTML = m + "<br/><span>Minutes</span>";
+  seconds.innerHTML = s + "<br/><span>Seconds</span>";
 
-    hh.style.strokeDashoffset = 440 - (440 * h) / 12; // for 12hr clock
-    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
-    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+  // set am or pm description
+  ampm.innerHTML = am;
 
-    hr_dot.style.transform = `rotate(${h * 30}deg)`;    
-    min_dot.style.transform = `rotate(${m * 6}deg)`;    
-    sec_dot.style.transform = `rotate(${s * 6}deg)`;    
+  hh.style.strokeDashoffset = 440 - (440 * h) / 12; // for 12hr clock
+  mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+  ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+  hr_dot.style.transform = `rotate(${h * 30}deg)`;
+  min_dot.style.transform = `rotate(${m * 6}deg)`;
+  sec_dot.style.transform = `rotate(${s * 6}deg)`;
 });
